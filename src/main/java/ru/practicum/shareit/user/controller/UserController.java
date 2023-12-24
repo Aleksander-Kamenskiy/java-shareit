bdb1502ecfc80c4b72b89aea42c6f89e0559452a
Aleksander Kamenskiy <Lektor97Shura@yandex.ru>
2023-12-24 14:54:32 +0300

    feat: Добавлен функционал 13 ТЗ.

---
+++ src/main/java/ru/practicum/shareit/user/controller/UserController.java
@@ -0,0 +1,44 @@
+package ru.practicum.shareit.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.service.UserServiceDtoImpl;
+
+import javax.validation.Valid;
+import java.util.List;
+
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+    private final UserServiceDtoImpl userService;
+
+    @PostMapping
+    public UserDto add(@Valid @RequestBody UserDto userDto) {
+        return userService.add(userDto);
+    }
+
+    @GetMapping("/{userId}")
+    public UserDto findById(@PathVariable Long userId) {
+        return userService.findById(userId);
+    }
+
+    @GetMapping
+    public List<UserDto> findAll() {
+        return userService.findAll();
+    }
+
+    @PatchMapping("/{userId}")
+    public UserDto update(@PathVariable Long userId, @RequestBody UserDto userDto) {
+        return userService.update(userId, userDto);
+    }
+
+    @DeleteMapping("/{userId}")
+    public void delete(@PathVariable Long userId) {
+        userService.delete(userId);
+    }
+}
